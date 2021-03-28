a52fc129b2fa12d05c319cc7af180e1cc603f11f
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-03-28 21:34:08 +0200

    Added LinkedList "CosmicSystem" with add, get and size functions

--- src/CosmicSystem.java
+++ src/CosmicSystem.java
@@ -1,46 +1,74 @@
 //This class represents a linked list for objects of class 'Body'
 public class CosmicSystem {
 
-    //TODO: Define variables.
+    private String name;
+    private Node head;
+
 
     // Initialises this system as an empty system with a name.
     public CosmicSystem(String name) {
-        //TODO: implement constructor.
+        this.name = name;
     }
 
     // Adds 'body' to the end of the list of bodies if the list does not already contain a
     // body with the same name as 'body', otherwise does not change the object state. The method
     // returns 'true' if the list was changed as a result of the call and 'false' otherwise.
     public boolean add(Body body) {
-        //TODO: implement method.
-        return false;
+        if (this.head != null) return head.addNode(body);
+        this.head = new Node(body);
+        return true;
     }
 
     // Returns the 'body' with the index 'i'. The body that was first added to the list has the
     // index 0, the body that was most recently added to the list has the largest index (size()-1).
     // Precondition: 'i' is a valid index.
     public Body get(int i) {
-        //TODO: implement method.
-        return null;
+        return this.head == null ? null : head.getBody(i);
     }
 
     // Returns the body with the specified name or 'null' if no such body exits in the list.
     public Body get(String name) {
-        //TODO: implement method.
-        return null;
+        return this.head == null ? null : head.getBody(name);
     }
 
     // Returns the body with the same name as the input body or 'null' if no such body exits in the list.
     public Body get(Body body) {
-        //TODO: implement method.
-        return null;
+        return this.head == null ? null : head.getBody(body.getName());
     }
 
     // returns the number of entries of the list.
     public int size() {
-        //TODO: implement method.
-        return -1;
+        return this.head == null ? 0 : this.head.size();
     }
 
-    //TODO: Define additional class(es) implementing the linked list (either here or outside class).
+
+    class Node {
+        public Body data;
+        public Node nextNode;
+
+        public Node(Body data) {
+            this.data = data;
+        }
+
+        public boolean addNode(Body body) {
+            if (body.getName().equals(this.data.getName())) return false;
+            if (this.nextNode != null) return this.nextNode.addNode(body);
+            this.nextNode = new Node(body);
+            return true;
+        }
+
+        public Body getBody(int i) {
+            return i==0 ? this.data : this.nextNode.getBody(--i);
+        }
+
+        public Body getBody(String name) {
+            if (this.data.getName().equals(name)) return this.data;
+            return this.nextNode == null ? null : this.nextNode.getBody(name);
+        }
+
+        public int size() {
+            return this.nextNode == null ? 1 : 1 + this.nextNode.size();
+        }
+    }
 }
+
