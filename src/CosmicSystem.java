3d93d948be3952e44852ba2137d45f58561b7809
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-04-14 11:52:13 +0200

    Changed LinkedList to double LinkedList Added "add by int"-method

--- src/CosmicSystem.java
+++ src/CosmicSystem.java
@@ -43,4 +43,64 @@ public class CosmicSystem {
     public int size() {
         return this.head == null ? 0 : this.head.size();
     }
+
+    // Inserts the specified 'body' at the specified position
+    // in this list if 'i' is a valid index and there is no body
+    // in the list with the same name as that of 'body'.
+    // Shifts the element currently at that position (if any) and
+    // any subsequent elements to the right (adds 1 to their
+    // indices). The first element of the list has the index 0.
+    // Returns 'true' if the list was changed as a result of
+    // the call, 'false' otherwise.
+    public boolean add(int i, Body body) {
+        if (i < 0) return false;
+        if (this.head != null) {
+            if (this.head.nameInList(body)) return false;
+            if (i==0) { // Replace first node with non-empty List
+                MyNode buffer = this.head;
+                this.head = new MyNode(body);
+                buffer.setPrevNode(this.head);
+                this.head.setNextNode(buffer);
+            }
+            return head.addNode(i, body);
+        }
+        if (i == 0) {
+            this.head = new MyNode(body);
+            return true;
+        }
+        return false;
+    }
+
+    //removes the body at index i from the list, if i is a valid index
+    //returns true if removal was done, and false otherwise (invalid index)
+    public boolean remove(int i) {
+        //TODO: implement method.
+        return false;
+    }
+
+    //removes a body from the list, if the list contains a body with the same name as the input body
+    //returns true if removal was done, and false otherwise (no body with the same name)
+    public boolean remove(Body body) {
+        //TODO: implement method.
+        return false;
+    }
+
+    // Returns a new list that contains the same elements as this list in reverse order. The list 'this'
+    // is not changed and only the references to the bodies are copied, not their content (shallow copy).
+    public CosmicSystem reverse() {
+        //TODO: implement method.
+        return null;
+    }
+
+    // Returns a readable representation with the name of the system and all bodies in order of the list.
+    // E.g.,
+    // Jupiter System:
+    // Jupiter, 1.898E27 kg, radius: 6.9911E7 m, position: [0.0,0.0,0.0] m, movement: [0.0,0.0,0.0] m/s.
+    // Io, 8.9E22 kg, radius: 1822000.0 m, position: [0.0,0.0,0.0] m, movement: [0.0,0.0,0.0] m/s.
+    //
+    //Hint: also use toString() in Body.java for this.
+    public String toString() {
+        //TODO: implement method.
+        return null;
+    }
 }
--- src/MyNode.java
+++ src/MyNode.java
@@ -1,16 +1,42 @@
 public class MyNode {
-    public Body data;
-    public MyNode nextNode;
+    private Body data;
+    private MyNode nextNode;
+    private MyNode prevNode;
+
 
     public MyNode(Body data) {
         this.data = data;
     }
 
+    public void setNextNode(MyNode node) {this.nextNode = node;}
+    public void setPrevNode(MyNode node) {this.prevNode = node;}
+
     public boolean addNode(Body body) {
         if (body.getName().equals(this.data.getName())) return false;
         if (this.nextNode != null) return this.nextNode.addNode(body);
         this.nextNode = new MyNode(body);
+        this.nextNode.prevNode = this;
         return true;
+    }
+
+    public boolean addNode(int i, Body body) {
+        if (i==1) {
+            MyNode buffer = this.nextNode;
+            this.nextNode = new MyNode(body);
+            this.nextNode.nextNode = buffer;
+            this.nextNode.prevNode = this;
+            if (this.nextNode.nextNode != null)
+                this.nextNode.nextNode.prevNode = this.nextNode;
+            return true;
+        }
+        if (this.nextNode == null) return false;
+        return this.nextNode.addNode(i-1, body);
+    }
+
+    public boolean nameInList(Body body) {
+        if (body.getName().equals(this.data.getName())) return true;
+        if (this.nextNode != null) return this.nextNode.nameInList(body);
+        return false;
     }
 
     public Body getBody(int i) {
