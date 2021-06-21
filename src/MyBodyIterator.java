fe9effdf2b276a41317354f067640b92eafd08ef
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2021-06-21 18:38:49 +0200

    Initial commit

---
+++ src/MyBodyIterator.java
@@ -0,0 +1,17 @@
+public class MyBodyIterator implements BodyIterator {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Body next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
